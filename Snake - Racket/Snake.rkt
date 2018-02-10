8a502c283ee914411dcb37b4f909f9ebaeb12d05
Andrew <sacarter@live.com>
2018-02-10 00:30:39 -0500

    Snake Now Moves

--- Snake - Racket/Snake.rkt
+++ Snake - Racket/Snake.rkt
@@ -52,8 +52,14 @@
   (draw-food (world-food world)(draw-snake (world-snake world) BACKGROUND)))
 
 ;; update-world: updates the current world one time
-(define (update-world world)
-  world)
+(define (update-world a-world)
+  (define snk (world-snake a-world))
+  (define food (world-food a-world))
+  (cond [(snake-ate? (world-snake a-world) (world-food a-world))
+         (set! snk (grow-and-move-snake (world-snake a-world)))
+         (set! food (random-loc))]
+        [else (set! snk (move-snake (world-snake a-world)))])
+  (world snk food (world-scene a-world)))
 
 ;; game-won?: determines if the game is over
 (define (game-won? world)
@@ -64,22 +70,24 @@
   world)
 
 
-;                                  
-;                                  
-;                                  
-;                                  
-;   ;;;;                           
-;    ;  ;                          
-;    ;   ;  ;; ;;;   ;;;;   ;;; ;;;
-;    ;   ;   ;;     ;    ;   ;   ; 
-;    ;   ;   ;       ;;;;;   ; ; ; 
-;    ;   ;   ;      ;    ;   ; ; ; 
-;    ;  ;    ;      ;   ;;   ; ; ; 
-;   ;;;;    ;;;;;    ;;; ;;   ; ;  
-;                                  
-;                                  
-;                                  
-;                                  
+
+;                                                                                                                  
+;                                                                                                                  
+;                                                                                                                  
+;                                                                                                                  
+;   ;;;;                                    ;;;;;;                                     ;                           
+;    ;  ;                                    ;   ;                           ;                                     
+;    ;   ;  ;; ;;;   ;;;;   ;;; ;;;          ; ;    ;;  ;;  ;; ;;    ;;; ;  ;;;;;    ;;;     ;;;;   ;; ;;    ;;;;; 
+;    ;   ;   ;;     ;    ;   ;   ;           ;;;     ;   ;   ;;  ;  ;   ;;   ;         ;    ;    ;   ;;  ;  ;    ; 
+;    ;   ;   ;       ;;;;;   ; ; ;           ; ;     ;   ;   ;   ;  ;        ;         ;    ;    ;   ;   ;   ;;;;  
+;    ;   ;   ;      ;    ;   ; ; ;           ;       ;   ;   ;   ;  ;        ;         ;    ;    ;   ;   ;       ; 
+;    ;  ;    ;      ;   ;;   ; ; ;           ;       ;  ;;   ;   ;  ;    ;   ;   ;     ;    ;    ;   ;   ;  ;    ; 
+;   ;;;;    ;;;;;    ;;; ;;   ; ;           ;;;       ;; ;; ;;; ;;;  ;;;;     ;;;    ;;;;;   ;;;;   ;;; ;;; ;;;;;  
+;                                                                                                                  
+;                                                                                                                  
+;                                                                                                                  
+;                                                                                                                  
+                                  
 
 ;; draw-snake: draws the snake onto the given scene
 ;; snake -> current snake object to draw
@@ -102,6 +110,69 @@
 (define (draw-food loc scene)
   (place-image/align FOOD (* (loc-x loc) CELL-SIZE) (* (loc-y loc) CELL-SIZE) "left" "top" scene))
 
+
+;                                                                                                                                  
+;                                                                                                                                  
+;                                                                                                                                  
+;                                                                                                                                  
+;   ;;; ;;;             ;;                                  ;;;;;;                                     ;                           
+;    ;   ;               ;           ;                       ;   ;                           ;                                     
+;    ;   ;  ;; ;;    ;;; ;   ;;;;   ;;;;;    ;;;;            ; ;    ;;  ;;  ;; ;;    ;;; ;  ;;;;;    ;;;     ;;;;   ;; ;;    ;;;;; 
+;    ;   ;   ;;  ;  ;   ;;  ;    ;   ;      ;    ;           ;;;     ;   ;   ;;  ;  ;   ;;   ;         ;    ;    ;   ;;  ;  ;    ; 
+;    ;   ;   ;   ;  ;    ;   ;;;;;   ;      ;;;;;;           ; ;     ;   ;   ;   ;  ;        ;         ;    ;    ;   ;   ;   ;;;;  
+;    ;   ;   ;   ;  ;    ;  ;    ;   ;      ;                ;       ;   ;   ;   ;  ;        ;         ;    ;    ;   ;   ;       ; 
+;    ;   ;   ;   ;  ;   ;;  ;   ;;   ;   ;  ;                ;       ;  ;;   ;   ;  ;    ;   ;   ;     ;    ;    ;   ;   ;  ;    ; 
+;     ;;;    ;;;;    ;;; ;;  ;;; ;;   ;;;    ;;;;;          ;;;       ;; ;; ;;; ;;;  ;;;;     ;;;    ;;;;;   ;;;;   ;;; ;;; ;;;;;  
+;            ;                                                                                                                     
+;           ;;;                                                                                                                    
+;                                                                                                                                  
+;
+
+
+;; move-snake: moves the snake in its current direction.
+
+(check-expect (move-snake (snake "up" (list (loc 2 2)))) (snake "up" (list (loc 2 1))))
+(check-expect (move-snake (snake "right" (list (loc 2 2)))) (snake "right" (list (loc 3 2))))
+(check-expect (move-snake (snake "down" (list (loc 2 2)))) (snake "down" (list (loc 2 3))))
+(check-expect (move-snake (snake "left" (list (loc 2 2) (loc 3 2)))) (snake "left" (list (loc 1 2) (loc 2 2))))
+
+(define (move-snake snk)
+  (define direction (snake-dir snk))
+  (define segments (snake-segments snk))
+  (define new-segments (map (λ (seg) (move-segment seg direction)) segments))
+  (snake (snake-dir snk) new-segments))
+
+;; move-segment: moves a single segment in given direction
+
+(check-expect (move-segment (loc 2 3) "up") (loc 2 2))
+(check-expect (move-segment (loc 2 3) "down") (loc 2 4))
+(check-expect (move-segment (loc 2 3) "left") (loc 1 3))
+(check-expect (move-segment (loc 2 3) "right") (loc 3 3))
+
+(define (move-segment segment dir)
+  (define x (loc-x segment))
+  (define y (loc-y segment))
+  (cond [(string=? dir "up") (set! y (- y 1))]
+        [(string=? dir "down") (set! y (+ y 1))]
+        [(string=? dir "left") (set! x (- x 1))]
+        [(string=? dir "right") (set! x (+ x 1))])
+  (loc x y))
+
+;; grow-snake: adds a segment to the snake
+
+(define (grow-and-move-snake snake)
+  snake)
+
+;; snake-ate: determines if the snake has eaten the food
+
+(check-expect (snake-ate? (snake "" (list (loc 2 2))) (loc 2 2)) #t)
+(check-expect (snake-ate? (snake "" (list (loc 3 4))) (loc 3 5)) #f)
+(check-expect (snake-ate? (snake "" (list (loc 3 4))) (loc 2 4)) #f)
+
+(define (snake-ate? snake food)
+  (define head (first (snake-segments snake)))
+  (and (= (loc-x head) (loc-x food))
+       (= (loc-y head) (loc-y food))))
 
 
 ;; Random Helper Functions
